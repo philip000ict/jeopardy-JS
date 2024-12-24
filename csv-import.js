c5835a25c55a49b2421f9b7b035e88f2d5f5958a
philip000ict <philip000ict@gmail.com>
2024-12-25 09:28:03 +1100

    update all files

--- csv-import.js
+++ csv-import.js
@@ -1,48 +1,13 @@
 
-let Names = ["img/diamond.png","Diamond","img/ruby.png","Ruby","img/emerald.png","Emerald","img/saphire.png","Saphire","img/opal.png","Opal","\
+let gemsarray = ["img/diamond.png","Diamond","img/ruby.png","Ruby","img/emerald.png","Emerald","img/saphire.png","Saphire","img/opal.png","Opal","\
     img/crystal.png","Crystal","img/garnet.png","Garnet","img/malachite.png","Malechite","img/lapis.png","Lapis-Lazuli","img/pyrites.png","Pyrites","\
-    img/topaz.png","Topaz","img/jasper.png","Jasper","img/beryl.png","Beryl","img/onyx.png","Onyx","img/chalcedony.png","Chalcedony"];
+    img/topaz.png","Topaz","img/jasper.png","Jasper","img/beryl.png","Beryl","img/onyx.png","Onyx","img/chalcedony.png","Chalced-ony"];
 
+let toysarray = ["play-toy/ball0sm.png","ball","play-toy/bearsm.png","bear","play-toy/bikesm.png","bike","play-toy/capsm.png","cap","play-toy/carsm.png","car","play-toy/dumptrucksm.png","dump truck","play-toy/fairysm.png","fairy","play-toy/firetrucksm.png","firetruck","play-toy/hatsm.png","hat","play-toy/lollipopsm.png","lollipop","play-toy/sockssm.png","socks","play-toy/shoesm.png","shoe","play-toy/shortssm.png","shorts","play-toy/tractorsm.png","tractor","play-toy/trucksm.png","truck","play-toy/tshirtsm.png","tshirt"];
 
-    // var fileUpload = document.getElementById("fileUpload");
-    // var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
-    // if (regex.test(fileUpload.value.toLowerCase())) {
-    //     if (typeof (FileReader) != "undefined") {
-    //         var reader = new FileReader();
-    //         reader.onload = function (e) {
-    //             var table = document.createElement("table");
-    //             var rows = e.target.result.split("\n");
-    //             for (var i = 0; i < rows.length; i++) {
-    //                 var row = table.insertRow(-1);
-    //                 var cells = rows[i].split(",");
-    //                 for (var j = 0; j < cells.length; j++) {
-    //                     var cell = row.insertCell(-1);
-    //                     cell.innerHTML = cells[j];
-    //                 }
-    //             }
-    //             var dvCSV = document.getElementById("dvCSV");
-    //             dvCSV.innerHTML = "";
-    //             dvCSV.appendChild(table);
-    //         }
-    //         reader.readAsText(fileUpload.files[0]);
-    //     } else {
-    //         alert("This browser does not support HTML5.");
-    //     }
-    // } else {
-    //     alert("Please upload a valid CSV file.");
-    // }
-
-
-
-// const fs = require('fs');
-// const csvParser = require('csv-parser');
-
-// fs.readFile('gems.csv', (err, data) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   const csvData = csvParser.parse(data.toString());
-//   console.log(csvData);
-//   // csvData is an array of objects, where each object represents a row
-// });
+let animalarray = ["img/animal/batsm.png","bat","img/animal/bunnysm.png","bunny","img/animal/catsm.png","cat","img/animal/clownfishsm.png","clownfish","img/animal/dogsm.png","dog","\
+    img/animal/dolphinsm.png","dolphin","img/animal/eaglesm.png","eagle","img/animal/emusm.png","emu","img/animal/fantailsm.png","fantail","img/animal/goldfishsm.png","goldfish","\
+    img/animal/horsesm.png","horse","img/animal/joeysm.png","joey","img/animal/kangaroosm.png","kangaroo","img/animal/kangaroosm.png","kangaroo","img/animal/kittensm.png","kitten","\
+    img/animal/owlsm.png","owl","img/animal/pelicansm.png","pelican","img/animal/penguinsm.png","penguin","img/animal/ponysm.png","pony","img/animal/puppysm.png","puppy","\
+    img/animal/rabbitsm.png","rabbit","img/animal/ratsm.png","rat","img/animal/seagullsm.png","seagull","img/animal/sealsm.png","seal","img/animal/whalesm.png","whale"];
+    
